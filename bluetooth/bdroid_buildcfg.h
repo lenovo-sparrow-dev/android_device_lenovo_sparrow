3f3b579c340f45dd605ef32054f4092ed61ddbc7
azwhikaru <37921907+azwhikaru@users.noreply.github.com>
2024-07-24 15:38:22 +0800

    sparrow: change bluetooth display name

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,7 +17,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME             "Lenovo StarView"
+#define BTM_DEF_LOCAL_NAME             "Lenovo Smart Display"
 
 #define BLE_VND_INCLUDED               TRUE
 #define BLUETOOTH_QTI_SW               TRUE
